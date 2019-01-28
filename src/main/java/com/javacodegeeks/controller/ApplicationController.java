c6bb00664eeab318f3fd369a70e7b9955d3b4bf0
njpbxa <30722316+njpbxa@users.noreply.github.com>
2019-01-28 19:36:18 +0530

    Jenkinsfile commit

--- src/main/java/com/javacodegeeks/controller/ApplicationController.java
+++ src/main/java/com/javacodegeeks/controller/ApplicationController.java
@@ -7,23 +7,23 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
- 
+
 @Controller
 @RequestMapping("/")
 public class ApplicationController {
- 
-	@RequestMapping(value="/Test", method = RequestMethod.GET)
-	public String welcome(ModelMap model) { 
+
+	@RequestMapping(value = "/Test", method = RequestMethod.GET)
+	public String welcome(ModelMap model) {
 		model.addAttribute("msgArgument", "Maven Java Web Application Project: Success!");
 
 		return "index";
 	}
- 
-	@RequestMapping(value="/Print/{arg}", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/Print/{arg}", method = RequestMethod.GET)
 	public String welcomeName(@PathVariable String arg, ModelMap model) {
 		model.addAttribute("msgArgument", "Maven Java Web Application Project, input variable: " + arg);
-		
+
 		return "index";
 	}
- 
+
 }
